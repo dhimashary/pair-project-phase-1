73bda15c6bcd1f4b5bd84185406f0e73434b6036
Ribka Haryuni <ribka.blue@gmail.com>
2018-10-25 16:35:07 +0700

    adding users view, bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const { Product } = require('../models')
+const { User } = require('../models')
 
 routes.get('/', (req,res)=> {
     res.send('masuk')
@@ -8,7 +9,7 @@ routes.get('/', (req,res)=> {
 routes.get('/admin/tickets', (req,res) => {
     Product.findAll()
            .then((data) => {
-            res.render('index', {path: './tickets/index', products:data})
+            res.render('index', {path: './tickets/user-regis.ejs', products:data})
            })
            .catch((err) => {
             res.send(err)
@@ -19,4 +20,24 @@ routes.get('/admin/tickets', (req,res) => {
 routes.get('/admin/tickets/add', (req,res) => {
     res.render('index', {path: './tickets/add'})
 })
+
+routes.get('/user/registration', (req, res) => { 
+    res.render("user/indexUser", {path: './user-regis.ejs', title: "User Registration"})
+})
+
+routes.post('/user/registration', (req, res) => { 
+    User.create({ 
+        username: req.body.username,
+        password: req.body.password,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    })
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
 module.exports = routes
